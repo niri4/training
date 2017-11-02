92164d929b88574f77bceb6728cbd6a643ced40f
niri4 <nirankarsingh247@gmail.com>
2017-11-02 12:37:53 +0530

    Exercise 11: Asking Questions

---
+++ exercise11.rb
@@ -0,0 +1,10 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "What's your name?"
+name = gets.chomp
+
+puts "So, you're Mr. #{name} your #{age} old, #{height} tall and #{weight} heavy."
