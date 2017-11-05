522824e0ee26d18c0d03018122c7001900b86f80
niri4 <nirankarsingh247@gmail.com>
2017-11-05 20:21:34 +0530

    Exercise 27: Memorizing Logic

---
+++ exercise27.rb
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+c = 10
+# return true when a is less than b
+puts a<b
+# return true when b is less than c
+puts b>c
+# return true when both are equal
+puts a==c
+# return false as a is equal to c but b is not equal to a
+puts a==c && b==a
+puts a==c || b==a
