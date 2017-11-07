a1246d492b5ae13797688e2db2e6083a7335f768
niri4 <nirankarsingh247@gmail.com>
2017-11-07 15:54:42 +0530

    Exercise 35: Branches and Functions

---
+++ exercise34.rb
@@ -0,0 +1,25 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+one = animals[1]
+print "The animal at 1 is %s" % one
+
+two = animals[3]
+print "The 3rd animal is %s" % two
+
+three = animals[0]
+print "The 1st animal is %s" % three
+
+four = animals[3]
+print "The animal at 3 is %s" % four
+
+five = animals[4]
+print "The 5th animal is a %s" % five
+
+six = animals[2]
+print "The animal at 2 is %s" % six
+
+seven = animals[5]
+print "The sixth animal is a %s" % seven
+
+eight = animals[4]
+print "The animal at four is a %s" % eight
