06632e0278d594632878b412f6e051a9017ccf40
niri4 <nirankarsingh247@gmail.com>
2017-11-08 13:34:52 +0530

    Exercise 43: Basic Object-Oriented Analysis and Design

---
+++ exercise42.rb
@@ -0,0 +1,86 @@
+class Animal
+end
+
+## class dog inherited with Animal class
+class Dog < Animal
+
+  def initialize(name)
+    ## ??
+    @name = name
+  end
+end
+
+## class cat inherited with Animal class
+class Cat < Animal
+
+  def initialize(name)
+    ## ??
+    @name = name
+  end
+end
+
+## ??
+class Person
+
+  def initialize(name)
+    ## ??
+    @name = name
+
+    ## Person has-a pet of some kind
+    @pet = nil
+  end
+
+  attr_accessor :pet
+end
+
+## ??
+class Employee < Person
+
+  def initialize(name, salary)
+    ## ?? hmm what is this strange magic?
+    super(name)
+    ## ??
+    @salary = salary
+  end
+
+end
+
+## ??
+class Fish
+end
+
+## ??
+class Salmon < Fish
+end
+
+## ??
+class Halibut < Fish
+end
+
+
+## rover is-a Dog
+rover = Dog.new("Rover")
+
+## ??
+satan = Cat.new("Satan")
+
+## ??
+mary = Person.new("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee.new("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish.new()
+
+## ??
+crouse = Salmon.new()
+
+## ??
+harry = Halibut.new()
